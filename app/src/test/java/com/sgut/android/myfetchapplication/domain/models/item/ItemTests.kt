b85b35fb70b07f9173fbfb8ae2f6cc260307ad8c
salgut408 <sgutie@saic.edu>
2023-02-27 13:40:15 -0600

    Test Started

---
+++ app/src/test/java/com/sgut/android/myfetchapplication/domain/models/item/ItemTests.kt
@@ -0,0 +1,28 @@
+package com.sgut.android.myfetchapplication.domain.models.item
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+
+class ItemTests{
+    private class Item(){
+        private val id = 123
+        private val listId = 321
+        private val name = "ItemName"
+    }
+
+    private val id = 123
+    private val listId = 321
+    private val name = "ItemName"
+
+
+
+
+    @Test
+    fun item_id_test() {
+        val item = Item()
+        val expectedName = name
+//        assertEquals
+    }
+
+}
