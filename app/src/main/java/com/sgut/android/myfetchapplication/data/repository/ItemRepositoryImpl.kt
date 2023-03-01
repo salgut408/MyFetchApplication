ac844be7e83739a112c215bf7ff7fb85868225fe
salgut408 <sgutie@saic.edu>
2023-02-28 23:59:50 -0600

    dao name refactor

--- app/src/main/java/com/sgut/android/myfetchapplication/data/repository/ItemRepositoryImpl.kt
+++ app/src/main/java/com/sgut/android/myfetchapplication/data/repository/ItemRepositoryImpl.kt
@@ -14,34 +14,21 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-//ItemRepositoryImpl Implementation
-
 class ItemRepositoryImpl @Inject constructor(
     val networkItemDtoMapperImpl: NetworkItemDtoMapperImpl,
     val dao: ItemDao,
     val api: FetchApi,
     val itemDatabase: ItemDatabase
 ): ItemsRepository {
-
-    //this gets sorted list from sql in db
-
-
     // this gets all info, not sorted
     override  suspend fun getAllInfoFromRepository(): Flow<List<ItemDomainModel>> {
-        val result = dao.getAllInfoFromDb()
-        return result
-
+        return  dao.getAllInfoFromDb()
     }
 
     //this gets all favorite items from favorites table in db
     override suspend fun getFavorites(): Flow<List<FavoriteItem>> {
-        val result = dao.getAllFavoriteItems()
-        return result
+       return dao.getAllFavoriteItems()
     }
-
-
-
-    //repository is central place for all data changes so this is where extension f(x)'s called
 
     override  suspend fun saveFavoriteItem(item: ItemDomainModel){
         itemDatabase.getDao().insertFavoriteItem(item.asFavoriteItem())
@@ -51,17 +38,15 @@ class ItemRepositoryImpl @Inject constructor(
         itemDatabase.getDao().delete(item.asFavoriteItem())
     }
 
-
-
-    //calls for all items to populate databaase no sorting
+    //calls for all items to populate database no sorting
     override suspend fun getInfoForDatabaseNoSort() {
         withContext(Dispatchers.IO) {
             try {
                 val items = api.getFetchInformation().body()
                     ?.let { networkItemDtoMapperImpl.toDomainList(it) }
-                items?.map{dao.update(it)}
+                items?.map{ dao.update(it) }
             } catch (err: Exception) {
-                Log.i("Tag", "Failed, ${err.message.toString()}")
+                Log.e("Repository", "Failed, ${err.message.toString()}")
             }
         }
     }
